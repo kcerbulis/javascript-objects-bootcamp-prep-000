06f38aa03b256ddcb0eeab1bbe80dbafb497ba11
reactive-queue-1834 <kcerbulis@gmail.com>
2019-02-07 20:20:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,3 +8,8 @@ function updatePlaylist(playlist, name, title){
   playlist[name] = title
   return playlist
 }
+
+function removeFromPlaylist(playlist, name){
+  delete playlist.name
+  return playlist
+}
